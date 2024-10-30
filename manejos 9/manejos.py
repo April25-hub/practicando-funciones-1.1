b4bb574437fff2ab193092051ec75e7a949e1c58
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-10-30 16:55:35 -0600

    Add files via upload

---
+++ manejos 9/manejos.py
@@ -0,0 +1,10 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+def my_function(country="Unknown"):
+    print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
