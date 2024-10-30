6fef4fbc6cc888aa235add4c8b0c26c3e417f246
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-10-30 15:37:45 -0600

    Add files via upload

---
+++ MANEJO DE LISTAS 4/manejo nde listas .py
@@ -0,0 +1,13 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+def suma(a, b):
+    return a + b
+
+#Llamada correcta a la función
+resultado = suma(5, 10)
+print(resultado)  
+#Salida: 15
+
+# Llamada incorrecta a la función (esto generará un error)
+# resultado = suma(5)  # TypeError: missing 1 required positional argument
